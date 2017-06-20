d6abed0bccf6ed63ff2d1c0c52e6fdb6e78b505c
Brian Russo <brusso@anexinet.com>
2017-06-20 08:19:28 -0400

    Initial commit.

---
+++ app/components/Nav.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+    render: function (){
+        return (
+            <div className="top-bar" id="example-menu">
+                <div className="top-bar-left">    
+                    <ul className="menu">
+                        <li className="menu-text">React Timer App</li>
+                        <li>
+                            <IndexLink to="/timer" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+                        </li>  
+                    </ul>      
+                </div>
+                <div className="top-bar-right">
+                        <ul className="menu">
+                            <li>
+                                <li className="menu-text">
+                                    Created by <a href="http://www.anexinet.com" target="_blank">Brian Russo</a>
+                                </li>
+                            </li>
+                        </ul>       
+                </div>
+            </div>        
+        )
+    }
+
+});
+
+module.exports = Nav;
+
